dc1c6d3a4ed487fb9c26f170f2a4af974fb72082
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2022-06-21 15:28:24 -0300

    push js

--- js/app.js
+++ js/app.js
@@ -9,17 +9,21 @@ const contenedorCarrito = document.getElementById('carrito-contenedor');
 const botonTerminar = document.getElementById('terminar');
 const finCompra = document.getElementById('fin-compra');
 
-const contadorCarrito = document.getElementById('contador-carrito');
+const contadorCarrito = document.getElementById('contadorCarrito');
 const precioTotal = document.getElementById('precioTotal');
 
 const buscador = document.getElementById('search');
 
 
-mostrarProductos()
+mostrarProductos1()
+mostrarProductos2()
+mostrarProductos3()
+mostrarProductos4()
+recuperar()
 
 
-// Logica Ecommerce
-function mostrarProductos() {
+// Funcion Ofertas Columna 1
+function mostrarProductos1() {
     stockProductosOfertaColumna1.forEach(el=> {
         let div = document.createElement('div')
         div.className = 'col border border-2 bg-light me-3'
@@ -27,16 +31,56 @@ function mostrarProductos() {
                             <p class="autos-tittle">${el.nombre}</p>
                             <p class="autos-price">Precio</p>
                             <p class="autos-tittle">$${el.precio}</p>
-                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Comprar</button>`
+                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Agregar al Carrito</button>`
         const padreColumna1 = document.querySelector('.column-1')
         padreColumna1.appendChild(div)
         let btnAgregar = document.getElementById(`boton${el.id}`)
-        btnAgregar.addEventListener('click', ()=>{
-            alert("Ha comprado "+`${el.nombre}`+" por el valor de $"+`${el.precio}`)
+        btnAgregar.addEventListener('click',()=>{
+            agregarAlCarrito1(el.id)
         })
-        
     })
+}
+function agregarAlCarrito1(id) {
+    let productoAgregar1 = stockProductosOfertaColumna1.find(item => item.id === id)
+    carritoDeCompras1.push(productoAgregar1)
+    mostrarCarrito(productoAgregar1)
+    actualizarCarrito1()
+    localStorage.setItem('carrito',JSON.stringify(carritoDeCompras1))
+}
+function mostrarCarrito(productoAgregar1) {
+    let div = document.createElement('div')
+    div.classList.add('carrito-oferta-1')
+    div.innerHTML =    `<p>${productoAgregar1.nombre}</p>
+                        <p>Precio: $${productoAgregar1.precio}</p>
+                        <button id=eliminar${productoAgregar1.id} class="boton-eliminar"><i class="bi bi-trash-fill"></i></button>`
+    const PadreCarritoOferta1 = document.querySelector('.carrito')
+    PadreCarritoOferta1.appendChild(div)
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar1.id}`)
+    btnEliminar.addEventListener('click', ()=>{
+        btnEliminar.parentElement.remove()
+        carritoDeCompras1 = carritoDeCompras1.filter(elemento => elemento.id !== productoAgregar1.id)
+        actualizarCarrito1()
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras1))
+    })
+}
+function actualizarCarrito1() {
+    contadorCarrito.innerText = carritoDeCompras1.length
+    precioTotal.innerText = carritoDeCompras1.reduce((acc, el)=> acc + el.precio, 0)
+}
+function recuperar() {
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+    if (recuperarLS) {
+        for (const elemento of recuperarLS) {
+            mostrarCarrito(elemento)
+            carritoDeCompras1.push(elemento)
+            actualizarCarrito1()
+        }
+    }
+}
 
+
+// Funcion Ofertas Columna 2
+function mostrarProductos2() {
     stockProductosOfertaColumna2.forEach(el=> {
         let div = document.createElement('div')
         div.className = 'col border border-2 bg-light me-3'
@@ -44,15 +88,56 @@ function mostrarProductos() {
                             <p class="autos-tittle">${el.nombre}</p>
                             <p class="autos-price">Precio</p>
                             <p class="autos-tittle">$${el.precio}</p>
-                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Comprar</button>`
-        const padreColumna1 = document.querySelector('.column-2')
-        padreColumna1.appendChild(div)
+                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Agregar al Carrito</button>`
+        const padreColumna2 = document.querySelector('.column-2')
+        padreColumna2.appendChild(div)
         let btnAgregar = document.getElementById(`boton${el.id}`)
-        btnAgregar.addEventListener('click', ()=>{
-            alert("Ha comprado "+`${el.nombre}`+" por el valor de $"+`${el.precio}`)
+        btnAgregar.addEventListener('click',()=>{
+            agregarAlCarrito2(el.id)
         })
     })
+}
+function agregarAlCarrito2(id) {
+    let productoAgregar2 = stockProductosOfertaColumna2.find(item => item.id === id)
+    carritoDeCompras2.push(productoAgregar2)
+    mostrarCarrito(productoAgregar2)
+    actualizarCarrito2()
+    localStorage.setItem('carrito',JSON.stringify(carritoDeCompras2))
+}
+function mostrarCarrito(productoAgregar2) {
+    let div = document.createElement('div')
+    div.classList.add('carrito-oferta-2')
+    div.innerHTML =    `<p>${productoAgregar2.nombre}</p>
+                        <p>Precio: $${productoAgregar2.precio}</p>
+                        <button id=eliminar${productoAgregar2.id} class="boton-eliminar"><i class="bi bi-trash-fill"></i></button>`
+    const PadreCarritoOferta2 = document.querySelector('.carrito')
+    PadreCarritoOferta2.appendChild(div)
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar2.id}`)
+    btnEliminar.addEventListener('click', ()=>{
+        btnEliminar.parentElement.remove()
+        carritoDeCompras2 = carritoDeCompras2.filter(elemento => elemento.id !== productoAgregar2.id)
+        actualizarCarrito2()
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras2))
+    })
+}
+function actualizarCarrito2() {
+    contadorCarrito.innerText = carritoDeCompras2.length
+    precioTotal.innerText = carritoDeCompras2.reduce((acc, el)=> acc + el.precio, 0)
+}
+function recuperar() {
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+    if (recuperarLS) {
+        for (const elemento of recuperarLS) {
+            mostrarCarrito(elemento)
+            carritoDeCompras2.push(elemento)
+            actualizarCarrito2()
+        }
+    }
+}
 
+
+// Funcion Ofertas Columna 3
+function mostrarProductos3() {
     stockProductosOfertaColumna3.forEach(el=> {
         let div = document.createElement('div')
         div.className = 'col border border-2 bg-light me-3'
@@ -60,15 +145,57 @@ function mostrarProductos() {
                             <p class="autos-tittle">${el.nombre}</p>
                             <p class="autos-price">Precio</p>
                             <p class="autos-tittle">$${el.precio}</p>
-                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Comprar</button>`
-        const padreColumna1 = document.querySelector('.column-3')
-        padreColumna1.appendChild(div)
+                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Agregar al Carrito</button>`
+        const padreColumna3 = document.querySelector('.column-3')
+        padreColumna3.appendChild(div)
         let btnAgregar = document.getElementById(`boton${el.id}`)
-        btnAgregar.addEventListener('click', ()=>{
-            alert("Ha comprado "+`${el.nombre}`+" por el valor de $"+`${el.precio}`)
+        btnAgregar.addEventListener('click',()=>{
+            agregarAlCarrito3(el.id)
         })
     })
+}
+function agregarAlCarrito3(id) {
+    let productoAgregar3 = stockProductosOfertaColumna3.find(item => item.id === id)
+    carritoDeCompras3.push(productoAgregar3)
+    mostrarCarrito(productoAgregar3)
+    actualizarCarrito3()
+    localStorage.setItem('carrito',JSON.stringify(carritoDeCompras3))
+}
+function mostrarCarrito(productoAgregar3) {
+    let div = document.createElement('div')
+    div.classList.add('carrito-oferta-3')
+    div.innerHTML =    `<p>${productoAgregar3.nombre}</p>
+                        <p>Precio: $${productoAgregar3.precio}</p>
+                        <button id=eliminar${productoAgregar3.id} class="boton-eliminar"><i class="bi bi-trash-fill"></i></button>`
+    const PadreCarritoOferta3 = document.querySelector('.carrito')
+    PadreCarritoOferta3.appendChild(div)
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar3.id}`)
+    btnEliminar.addEventListener('click', ()=>{
+        btnEliminar.parentElement.remove()
+        carritoDeCompras3 = carritoDeCompras3.filter(elemento => elemento.id !== productoAgregar3.id)
+        actualizarCarrito3()
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras3))
+    })
+}
+function actualizarCarrito3() {
+    contadorCarrito.innerText = carritoDeCompras3.length
+    precioTotal.innerText = carritoDeCompras3.reduce((acc, el)=> acc + el.precio, 0)
+}
+function recuperar() {
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+    if (recuperarLS) {
+        for (const elemento of recuperarLS) {
+            mostrarCarrito(elemento)
+            carritoDeCompras3.push(elemento)
+            actualizarCarrito3()
+        }
+    }
+}
 
+
+
+// Funcion Ofertas Columna 4
+function mostrarProductos4() {
     stockProductosOfertaColumna4.forEach(el=> {
         let div = document.createElement('div')
         div.className = 'col border border-2 bg-light me-3'
@@ -76,13 +203,52 @@ function mostrarProductos() {
                             <p class="autos-tittle">${el.nombre}</p>
                             <p class="autos-price">Precio</p>
                             <p class="autos-tittle">$${el.precio}</p>
-                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Comprar</button>`
-        const padreColumna1 = document.querySelector('.column-4')
-        padreColumna1.appendChild(div)
+                            <button id="boton${el.id}" type="submit" class="btn btn-danger mb-3"></a>Agregar al Carrito</button>`
+        const padreColumna4 = document.querySelector('.column-4')
+        padreColumna4.appendChild(div)
         let btnAgregar = document.getElementById(`boton${el.id}`)
-        btnAgregar.addEventListener('click', ()=>{
-            alert("Ha comprado "+`${el.nombre}`+" por el valor de $"+`${el.precio}`)
+        btnAgregar.addEventListener('click',()=>{
+            agregarAlCarrito4(el.id)
         })
     })
 }
+function agregarAlCarrito4(id) {
+    let productoAgregar4 = stockProductosOfertaColumna4.find(item => item.id === id)
+    carritoDeCompras4.push(productoAgregar4)
+    mostrarCarrito(productoAgregar4)
+    actualizarCarrito4()
+    localStorage.setItem('carrito',JSON.stringify(carritoDeCompras4))
+}
+function mostrarCarrito(productoAgregar4) {
+    let div = document.createElement('div')
+    div.classList.add('carrito-oferta-2')
+    div.innerHTML =    `<p>${productoAgregar4.nombre}</p>
+                        <p>Precio: $${productoAgregar4.precio}</p>
+                        <button id=eliminar${productoAgregar4.id} ><i class="bi bi-trash-fill"></i></button>`
+    const PadreCarritoOferta4 = document.querySelector('.carrito')
+    PadreCarritoOferta4.appendChild(div)
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar4.id}`)
+    btnEliminar.addEventListener('click', ()=>{
+        btnEliminar.parentElement.remove()
+        carritoDeCompras4 = carritoDeCompras4.filter(elemento => elemento.id !== productoAgregar4.id)
+        actualizarCarrito4()
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras4))
+    })
+}
+function actualizarCarrito4() {
+    contadorCarrito.innerText = carritoDeCompras4.length
+    precioTotal.innerText = carritoDeCompras4.reduce((acc, el)=> acc + el.precio, 0)
+}
+function recuperar() {
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+    if (recuperarLS) {
+        for (const elemento of recuperarLS) {
+            mostrarCarrito(elemento)
+            carritoDeCompras4.push(elemento)
+            actualizarCarrito2()
+        }
+    }
+}
+
+
 
---
+++ js/stock-ofertas.js
@@ -0,0 +1,14 @@
+let stockOfertas = [
+    {id: 1, nombre: "Chevrolet S10", precio: 7964000, img: '../fotos/oferta-1.png', tipo: "oferta"},
+    {id: 2, nombre: "Chevrolet Onix Plus", precio: 3171740, img: '../fotos/oferta-2.png', tipo: "oferta"},
+    {id: 3, nombre: "Chevrolet Onix Plus", precio: 3525900, img: '../fotos/oferta-3.png', tipo: "oferta"},
+    {id: 4, nombre: "Chevrolet Onix", precio: 3523000, img: '../fotos/oferta-4.png', tipo: "oferta"},
+    {id: 5, nombre: "Peugeot 3008", precio: 10501432, img: '../fotos/oferta-5.png', tipo: "oferta"},
+    {id: 6, nombre: "Fiat Cronos", precio: 2780000, img: '../fotos/oferta-6.png', tipo: "oferta"},
+    {id: 7, nombre: "Peugeot Boxer", precio: 5960128, img: '../fotos/oferta-7.png', tipo: "oferta"},
+    {id: 8, nombre: "Fiat Cronos", precio: 3112000, img: '../fotos/oferta-8.png', tipo: "oferta"},
+    {id: 9, nombre: "Peugeot 208", precio: 3850424, img: '../fotos/oferta-9.png', tipo: "oferta"},
+    {id: 10, nombre: "Peugeot Partner", precio: 3029584, img: '../fotos/oferta-10.png', tipo: "oferta"},
+    {id: 11, nombre: "Peugeot 2008", precio: 4250541, img: '../fotos/oferta-11.png', tipo: "oferta"},
+    {id: 12, nombre: "Peugeot Expert", precio: 4935616, img: '../fotos/oferta-12.png', tipo: "oferta"},
+]
